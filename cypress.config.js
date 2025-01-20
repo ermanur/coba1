6851e7d1142670839410a64d69aa04026b2b5256
Erma Nursetyani <setyaninurerma@gmail.com>
2025-01-20 23:01:11 +0700

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,18 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://magento.softwaretestingboard.com/'
-    },
+    
+    baseUrl: 'https://magento.softwaretestingboard.com/',
+    browser: 'chrome',
+    //specPattern: 'cypress/e2e/web-magento/create*.cy.js',
+    specPattern: 'cypress/e2e/web-magento/**.cy.js',
+    screenshotOnRunFailure: false,
+    //chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     viewportHeight: 650,
-    viewportWidth: 950,
-    defaultCommandTimeout:5000
-
+    viewportWidth: 1000,
+    defaultCommandTimeout: 5000,
   },
-);
+});
